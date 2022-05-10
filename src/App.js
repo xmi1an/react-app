b31e53ec8bc064779c6ffa6bdf7ea2263e289fc5
mi1an <jadavmilan009@gmail.com>
2022-05-10 19:35:17 +0530

    first release

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import User from "./User";
 function App() {
   return (
     <div className="App">
@@ -17,6 +17,7 @@ function App() {
         >
           Learn React
         </a>
+        <User />
       </header>
     </div>
   );
---
+++ src/User.js
@@ -0,0 +1,11 @@
+function User() {
+  return (
+    <div>
+      <h1>I am User</h1>
+      <button type="button" class="btn btn-primary">
+        Primary
+      </button>
+    </div>
+  );
+}
+export default User;
